bd36e762e4fb5965e1e1484a12ea7812be959a8b
sravanichinna <sravanidane413.com>
2020-11-23 23:38:11 +0530

    Added: TotalWorkHours logic

--- empWageComputation.java
+++ empWageComputation.java
@@ -2,38 +2,34 @@ import java.util.Random;
 public class empWageComputation {
         public static void main(String args[])
         {
-             Random random=new Random();
+                int maxHrsInMonth=100;
+		int noOfWorkDay=20;
+		int empRatePerHr=20;
+		int isPartTime=1;
+		int isFullTime=2;
+		int empHrs=0;
+		int totalEmpHrs=0;
+		int totalWorkDay=0;
+		int totalEmpWage=0;
+	while( totalWorkDay<noOfWorkDay && totalEmpHrs<=maxHrsInMonth)
+	{
+		 Random random=new Random();
                 int randomNum=random.nextInt(3);
                 System.out.println("Random number: "+randomNum);
-                int fullTimePresent=2;
-                int partTimePresent=1;
-		int absent=0;
-		int wagePerHour=20;
-		int fullTimeHour=8;
-		int partTimeHour=4;
-		int dailyEmpWage=0;
-		int NumberOfWorkingDays=20;
-		int totalSalary=0;
-		int day=0;
-	for(day=1;day<=NumberOfWorkingDays;day++)
+	switch(randomNum)
 	{
-		switch(randomNum)
-		{
-		case 1:
-			int partTimeWage=(partTimeHour*wagePerHour);
-			System.out.println("part time employee: "+partTimeWage);
-			int totalWage=totalSalary+partTimeWage;
-			System.out.println("total salary: "+totalWage);
-      				break;
-		case 2:
-			int fullTimeWage=(fullTimeHour*wagePerHour);
-			System.out.println("full time employee: "+fullTimeWage);
-			int finalWage=totalSalary+fullTimeWage;
-			System.out.println("total Salary: "+finalWage);
-				break;
-		default:
-			System.out.println("Absent: "+absent);
-		}
+	case isPartTime: int empHrs=4;
+		break;
+	case isFullTime: int empHrs=8;
+		break;
+	default; int empHrs=0;
 	}
+		totalWorkDays++;
+		totalEmpHrs=totalEmpHrs + empHrs;
+		System.out.println("employee days: "+totalWorkDay + "work hours: "+totalEmpHrs);
+	}
+totalEmpWage=(toatalEmpHrs * empRatePerHour);
+System.out.println("employee total salary is: "+totalEmpWage);
+
 	}
 }
