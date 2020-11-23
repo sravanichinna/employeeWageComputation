0de2bb123627d3a2b9a1cd9aff0e813294136b3f
sravanichinna <sravanidane413.com>
2020-11-23 21:01:23 +0530

    Added: EmployeeAttendance logic

--- empWageComputation.java
+++ empWageComputation.java
@@ -1,8 +1,18 @@
-public class empWageComputation{
-	public static void main(String[] args) {
-
-
-
-		System.out.println("welcome to employee wage computation");
-	}
+import java.util.Random;
+public class empWageComputation {
+        public static void main(String args[])
+        {
+                Random random=new Random();
+                int randomNum=random.nextInt(2);
+                System.out.println("Random number: "+randomNum);
+                int isPresent=1;
+                if(randomNum==isPresent)
+                {
+                        System.out.println("employee is present");
+                }
+                else
+                {
+                        System.out.println("employee is absent");
+                }
+        }
 }
